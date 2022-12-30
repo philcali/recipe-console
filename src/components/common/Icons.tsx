d0661df6829e90668e86671d7530644b2b132680
Philip Cali <philip.cali@gmail.com>
2022-12-30 17:19:57 -0500

    chore: getting basic things in

---
+++ src/components/common/Icons.tsx
@@ -0,0 +1,40 @@
+const DEFAULT_RATIO = 16;
+
+type RatioIcon = (ratio: number) => React.SVGProps<SVGSVGElement>;
+
+class Icons {
+    readonly icons: {[name: string]: RatioIcon};
+
+    constructor() {
+        this.icons = {
+            'cart': ratio => {
+                return (
+                    <svg xmlns="http://www.w3.org/2000/svg" width={ratio} height={ratio} fill="currentColor" className="bi bi-cart4" viewBox="0 0 16 16">
+                        <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+                    </svg>
+                );
+            },
+            'share': ratio => {
+                return (
+                    <svg xmlns="http://www.w3.org/2000/svg" width={ratio} height={ratio} fill="currentColor" className="bi bi-share" viewBox="0 0 16 16">
+                        <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z"/>
+                    </svg>
+                );
+            },
+            'cloud-upload': ratio => {
+                return (
+                    <svg xmlns="http://www.w3.org/2000/svg" width={ratio} height={ratio} fill="currentColor" className="bi bi-cloud-arrow-up" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M7.646 5.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707L6.354 7.854a.5.5 0 1 1-.708-.708l2-2z"/>
+                        <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z"/>
+                    </svg>
+                )
+            }
+        }
+    }
+
+    icon(name: string, ratio?: number): React.SVGProps<SVGSVGElement> {
+        return this.icons[name](ratio || DEFAULT_RATIO);
+    }
+}
+
+export const icons = new Icons();
---
+++ src/pages/Home.tsx
@@ -0,0 +1,33 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { icons } from "../components/common/Icons";
+
+function Home() {
+    return (
+        <>
+            <div className="p-5 mb-4 bg-light rounded-3">
+                <Container>
+                    <h1 className="display-3">My Recipes</h1>
+                    <p>A collection of my recipes that cover food, drinks, and anything else with ingredients.</p>
+                </Container>
+            </div>
+            <Container>
+                <Row>
+                    <Col md>
+                        <h2><span><>{icons.icon('cloud-upload', 32)}</></span> Centralize</h2>
+                        <p>Store all of your recipes in a single place.</p>
+                    </Col>
+                    <Col md>
+                        <h2><span style={{verticalAlign: 'text-bottom'}}><>{icons.icon('cart', 32)}</></span> Shop</h2>
+                        <p>Create shopping lists from your recipes.</p>
+                    </Col>
+                    <Col md>
+                        <h2><span><>{icons.icon('share', 32)}</></span> Share</h2>
+                        <p>Share your recipes with friends and family.</p>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
+}
+
+export default Home;
